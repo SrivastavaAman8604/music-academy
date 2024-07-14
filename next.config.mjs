2d9ebfae618e41ac994de5fa274a733d7fc12a40
SrivastavaAman8604 <aman.srivastava2369@gmail.com>
2024-07-14 18:05:30 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['images.unsplash.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
+//   /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructor from "@/components/Instructor";
+import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="main-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
+    <HeroSection/>
+    <FeaturedCourses/>
+    <WhyChooseUs/>
+    <TestimonialCard/>
+    <UpcomingWebinars/> 
+    <Instructor/>
+    <Footer/>
+    </main>
+  );
+}
